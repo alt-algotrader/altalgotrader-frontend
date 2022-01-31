fe8cd5fce1dd96655e76525f8e7633e2611b9a73
Phillip <coin-trader-kr@gmail.com>
2022-01-31 23:36:47 +0900

    mobile specific text resizing implemented

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -93,7 +93,7 @@ const Banner = () => {
                         },
                         margin: {l: 50, r: 50, b: 50, t: 50, pad: 10},
                     }}
-                    config={{displayModeBar: false, scrollZoom: false}}
+                    config={{displayModeBar: false, scrollZoom: true}}
                 />
             </div>
         </>
@@ -106,11 +106,14 @@ const About = () => {
         <>
             <div className="home__about">
                 <div className="home__about--selection">
-                    <div className="home__about--selection-title">
+                    <div className="home__about--selection-title web-only">
                         <p> ___  __    __    __ </p>
                         <p>(  ,)(  )  /  \  / _)</p>
                         <p> ) ,\ )(__( () )( (/\</p>
                         <p>(___/(____)\__/  \__/</p>
+                    </div>
+                    <div className="home__about--selection-title mobile-only">
+                        BLOG
                     </div>
                     <div className="home__about--selection-description">
                         <p>알고리즘 트레이딩에 관련된 블로그글을 작성하여 올리고 있습니다.</p>
@@ -118,15 +121,33 @@ const About = () => {
                     </div>
                 </div>
                 <div className="home__about--selection">
-                    <div className="home__about--selection-title">
+                    <div className="home__about--selection-title web-only">
                         <p> ___   __  ____  __  </p>
                         <p>(   \ (  )(_  _)(  ) </p>
                         <p> ) ) )/__\  )(  /__\ </p>
                         <p>(___/(_)(_)(__)(_)(_)</p>
                     </div>
+                    <div className="home__about--selection-title mobile-only">
+                        DATA
+                    </div>
                     <div className="home__about--selection-description">
                         <p>실제 트레이딩에 사용하고 있는 데이터를 API로 제공드립니다.</p>
                         <p>주식, 선물, 암호화폐, 디파이 데이터 등 다양한 데이터가 있습니다.</p>
+                    </div>
+                </div>
+                <div className="home__about--selection">
+                    <div className="home__about--selection-title web-only">
+                        <p> ___   __   __  _ _  ____  ___  ___  ____ </p>
+                        <p>(  ,) (  ) / _)( ) )(_  _)(  _)/ __)(_  _)</p>
+                        <p> ) ,\ /__\( (_  )  \  )(   ) _)\__ \  )(  </p>
+                        <p>(___/(_)(_)\__)(_)\_)(__) (___)(___/ (__) </p>
+                    </div>
+                    <div className="home__about--selection-title mobile-only">
+                        BACKTEST
+                    </div>
+                    <div className="home__about--selection-description">
+                        <p>다양한 전략의 백테스팅 결과를 확인한 후 로버스트한 전략을 찾아볼 수 있습니다.</p>
+                        <p>실제 트레이딩에 사용중인 전략부터 리서치 전략까지 모두 소개드리겠습니다.</p>
                     </div>
                 </div>
             </div>
